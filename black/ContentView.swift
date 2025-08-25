f91fa4d3562970f30009f70ed7fe5fde2513696b
liukang <974004612@qq.com>
2025-08-25 23:37:44 +0800

    xcode commit

---
+++ black/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  black
+//
+//  Created by liukang on 2025/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ black/blackApp.swift
@@ -0,0 +1,17 @@
+//
+//  blackApp.swift
+//  black
+//
+//  Created by liukang on 2025/8/25.
+//
+
+import SwiftUI
+
+@main
+struct blackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

8929fe470caf1326ec7e31045d911a1fa3fdbbc4
K423 <974004612@qq.com>
2025-08-31 17:47:40 +0800

    xcode commit

---
+++ blackTests/blackTests.swift
@@ -0,0 +1,17 @@
+//
+//  blackTests.swift
+//  blackTests
+//
+//  Created by liukang on 2025/8/31.
+//
+
+import Testing
+@testable import black
+
+struct blackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ blackUITests/blackUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  blackUITestsLaunchTests.swift
+//  blackUITests
+//
+//  Created by liukang on 2025/8/31.
+//
+
+import XCTest
+
+final class blackUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
